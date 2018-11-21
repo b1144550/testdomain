2ca192eb3ce3601abaa3f1990e50244373e7e34c
root <root@sl-test-hk01.tlb-cloud.com>
2018-11-20 19:48:08 -0600

    add color and blink

--- curl.sh
+++ curl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "check http :1"
-echo "check https :2"
+
+echo -e "check \e[30;48;5;82m http \e[0m :1"
+echo -e "check \e[30;48;5;99m https \e[0m :2"
 echo "custom :3"
 read -p "(input 1 2 3):" method
 
@@ -20,22 +21,25 @@ case "$method" in
 	for ((i=1;i<=${#lines[@]};i++))
 	do
         #httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "https response code: %{response_code}")
-        httpcode=$(curl -sL http://"${lines[$i]}" -o /dev/null -w "http response code: %{response_code}")
+        httpcode=$(curl -sL http://"${lines[$i]}" -o /dev/null -w "code: %{response_code}")
         #http=$(curl -sL http://"${lines[$i]}" | grep title > /dev/null )
        # https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
         
         #if [  "$http" != null ]
         if  curl -sL http://"${lines[$i]}" | grep class > /dev/null  
         then
-            echo "live: ${lines[$i]} " ,"$httpcode"
+            echo -e "\e[30;48;5;82m ${lines[$i]} \e[0m" ,"$httpcode"
+
         else
-            echo -e "\e[31mdead\e[0m: ${lines[$i]} " ,"$httpcode"
+            echo -e "\e[5m\e[30;48;5;196m ${lines[$i]} \e[0m " ,"$httpcode"
         fi
         
 
 	done
     
 	;;
+
+###################################2################################################
 
 	2)
 	file=list
@@ -50,31 +54,30 @@ case "$method" in
 	
 	for ((i=1;i<=${#lines[@]};i++))
 	do
-        #httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "https response code: %{response_code}")
-        httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "https response code: %{response_code}")
-        https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
-       # https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
+        httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "code: %{response_code}")
+#        https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
         
         if curl -sL https://"${lines[$i]}" | grep class > /dev/null 
         then
-            echo "live: ${lines[$i]}" ,"$httpscode"
+            echo -e "\e[30;48;5;82m ${lines[$i]} \e[0m", "$httpscode"
         else
-            echo -e "\e[31mdead\e[0m: ${lines[$i]} " ,"$httpscode"
+            echo -e "\e[5m\e[30;48;5;196m ${lines[$i]} \e[0m", "$httpscode"
         fi
         
 
 	done
     
 	;;
-
+###############################################3#####################################################
     3)
 	read -p "input domain: " domain
+        httpcode=$(curl -sL "$domain" -o /dev/null -w "code: %{response_code}")
 
-	if curl -Ls --negotiate "$domain" | grep title > /dev/null
+	if curl -Ls --negotiate "$domain" | grep class > /dev/null
         then
-                echo "URL exists: $domain"
+                echo -e "\e[30;48;5;82m $domain \e[0m", "$httpcode"
         else
-                echo "URL dead: $domain"
+                echo -e "\e[5m\e30;48;5;196m $domain \e[0m", "$httpcode"
 	fi
 	;;
 
