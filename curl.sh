70029dbbc693c39b0f2a3302e28c9c2448199c14
HSY <hsy@HSYnoMacBook-Pro.local>
2018-11-22 23:24:25 +0800

    add function

--- curl.sh
+++ curl.sh
@@ -1,89 +1,67 @@
 #!/bin/bash
 
 
-echo -e "check \e[30;48;5;82m http \e[0m :1"
-echo -e "check \e[30;48;5;99m https \e[0m :2"
+echo -e "check \033[30;48;5;82m http \033[0m :1"
+echo -e "check \033[30;48;5;99m https \033[0m :2"
 echo "custom :3"
 read -p "(input 1 2 3):" method
 
+function read_list(){
+	file=list
+	seq=1
+	while read line
+	do
+		lines[$seq]=$line
+		((seq++))
+	done < $file
+}
+
+
+
 case "$method" in
 	1)
-	file=list
-	seq=1
-	while read line
-	do
-		lines[$seq]=$line
-		((seq++))
-	done < $file
-
-
-	
+	read_list
 	for ((i=1;i<=${#lines[@]};i++))
 	do
-        #httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "https response code: %{response_code}")
-        httpcode=$(curl -sL http://"${lines[$i]}" -o /dev/null -w "%{response_code}")
-        #http=$(curl -sL http://"${lines[$i]}" | grep title > /dev/null )
-       # https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
-        
-        #if [  "$http" != null ]
+        httpcode=$(curl -sL http://"${lines[$i]}" -o /dev/null -w "%{response_code}")        
         if  curl -sL http://"${lines[$i]}" | grep class > /dev/null  
         then
-            echo -e "\e[30;48;5;82m ${lines[$i]} \e[0m" ,"$httpcode"
-
+            echo -e "\033[30;48;5;82m ${lines[$i]} \033[0m" ,"$httpcode"
         else
-            echo -e "\e[5m\e[30;48;5;196m ${lines[$i]} \e[0m " ,"$httpcode"
+            echo -e "\033[5m\033[30;48;5;196m ${lines[$i]} \033[0m " ,"$httpcode"
         fi
-        
-
 	done
-    
 	;;
-
-###################################2################################################
+###################################2###################################################################
 
 	2)
-	file=list
-	seq=1
-	while read line
-	do
-		lines[$seq]=$line
-		((seq++))
-	done < $file
-
-
-	
+	read_list
 	for ((i=1;i<=${#lines[@]};i++))
 	do
-        httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "%{response_code}")
-#        https=$(curl -sL https://"${lines[$i]}" | grep title > /dev/null )
-        
+        httpscode=$(curl -sL https://"${lines[$i]}" -o /dev/null -w "%{response_code}")        
         if curl -sL https://"${lines[$i]}" | grep class > /dev/null 
         then
-            echo -e "\e[30;48;5;82m ${lines[$i]} \e[0m", "$httpscode"
+            echo -e "\033[30;48;5;82m ${lines[$i]} \033[0m", "$httpscode"
         else
-            echo -e "\e[5m\e[30;48;5;196m ${lines[$i]} \e[0m", "$httpscode"
+            echo -e "\033[5m\033[30;48;5;196m ${lines[$i]} \033[0m", "$httpscode"
         fi
-        
-
 	done
-    
 	;;
 ###############################################3#####################################################
     3)
 	read -p "input domain: " domain
-        httpcode=$(curl -sL "$domain" -o /dev/null -w "%{response_code}")
-
-	if curl -Ls --negotiate "$domain" | grep class > /dev/null
+    httpcode=$(curl -sL "$domain" -o /dev/null -w "%{response_code}")
+	if curl -Ls  "$domain" | grep class > /dev/null
         then
-                echo -e "\e[30;48;5;82m $domain \e[0m", "$httpcode"
+            echo -e "\033[30;48;5;82m $domain \033[0m", "$httpcode"
         else
-                echo -e "\e[5m\e30;48;5;196m $domain \e[0m", "$httpcode"
+            echo -e "\033[5m\033[30;48;5;196m $domain \033[0m", "$httpcode"
 	fi
 	;;
 
 	*)
 	exit 1
-		;;
+	;;
 esac
 
 
